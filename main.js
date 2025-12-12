8df6a03d0f71919c709d0a6728cb7337e305c334
Pascal <e273616@edu.sbl.ch>
2025-12-12 13:51:44 +0000

    Versuch: Hamburger-Icon (Versuch fehlgeschlagen) Schlussendlich Zähler gemacht Zähler WIP: Und versucht / überlegt in Aside etwas zu machen
    12.12.2025 14:51

--- main.js
+++ main.js
@@ -9,3 +9,21 @@ function createNewElement() {
   let mainElement = document.querySelector("main");
   mainElement.appendChild(newElement);
 }
+
+let titleElement = document.querySelector("#Start");
+
+let originalTitle = titleElement.textContent;
+
+let count = 0;
+
+function increaseCount() {
+  count = count + 1;
+
+  titleElement.textContent = originalTitle + " (" + count + ")";
+}
+
+document.addEventListener("keydown", increaseCount);
+
+titleElement.addEventListener("click", increaseCount);
+
+setInterval(increaseCount, 1000);
